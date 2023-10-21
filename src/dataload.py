058cb2aef293b56bd7d8d7936716d0697d22c87a
Ema <emanuel-0@hotmail.com>
2023-10-21 00:22:48 -0300

    asd

--- src/dataload.py
+++ src/dataload.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import numpy as np
 import librosa
+from src.utils import ekmans_thresholds
+
+def ekman_emotion(valence : float, arousal : float, dominance : float) -> str:
+
+    best_emotion = None
+    min_distance = float('inf')
+
+    # Calculate the Euclidean distance from the VAD values to each emotion's threshold
+    for emotion, (v_thresh, a_thresh, d_thresh) in ekmans_thresholds.items():
+        distance = ((valence - v_thresh) ** 2 + (arousal - a_thresh) ** 2 + (dominance - d_thresh) ** 2) ** 0.5 
+        
+        # Update the best emotion if the current emotion is closer
+        if distance < min_distance:
+            best_emotion = emotion
+            min_distance = distance 
+    
+    return best_emotion
+
 
 def load_audio_data(df_annotations: pd.DataFrame, pc_num: int, part_num: int) -> (np.ndarray, int):
 
